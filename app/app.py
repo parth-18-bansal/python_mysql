5d7a4b8a05b7a9bb3ce42cf371cc5a7824c3d61b
parth-18-bansal <parthbansalcoding@gmail.com>
2025-02-13 17:58:21 +0530

    deployed the multi container application with monitoring on docker

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 import mysql.connector
 import os
+from prometheus_client import start_http_server, Counter
+# # #import logging
 
 app = Flask(__name__)
 
+# #database connection
 DB_HOST = os.getenv("DB_HOST","localhost")
 DB_USER = os.getenv("DB_USER","root")
 DB_PASSWORD = os.getenv("DB_PASSWORD","root")
@@ -18,8 +21,18 @@ def connect_to_db():
     )
     return connect
 
+# # Create a counter metric to track requests
+REQUESTS = Counter('http_requests_total', 'Total number of HTTP requests')
+
+# # logging
+# #logging.basicConfig(filename='app.log',level=logging.INFO)
+
 @app.route('/')
 def home():
+    REQUESTS.inc()
+    
+    app.logger.info("Home Page")
+
     connect = connect_to_db()
     cursor = connect.cursor()
 
@@ -37,7 +50,18 @@ def home():
     return jsonify({"messages": messages})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    start_http_server(8000)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
 
+# when we use the debug = True, the app will reload itself when we make changes to the code. so if we use the debug = True and not
+# not add use_reloader = False, the app will reload itself and the prometheus server will start again and again.
+# first time it get bind with 8000 then debug also try to bind the prometheus server with 8000 but it is already binded so it will
+# throw an error. so to avoid this we use use_reloader = False. so that prometheus server will not start again and again.
+# or we will remove the debug = True. so that the app will not reload itself. so the prometheus server will not start again and again.
+
+
+# to check the process which is using a particular port. = sudo lsof -i :9323
+
+# to kill the process which is using a particular port. = sudo kill -9 <PID>
 
 
